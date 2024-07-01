bf240167296c89364396ad7aece3e71c6818c2f2
Chen Nissim <Chen.Nissim@niskoelec.com>
2024-07-01 16:45:42 +0300

    wip

--- src/screens/acceptance/AcceptanceScreen.tsx
+++ src/screens/acceptance/AcceptanceScreen.tsx
@@ -23,10 +23,10 @@ const AcceptanceScreen = () => {
 
         const steps: StepItem[] = []
 
-        data.map((a: Article) => {
-            const { body, title, id } = a
-            const lines = body.slice(0, 100)
-            steps.push({ id, label: title, description: lines, author: a.author, ttl: a.ttl })
+        data.map((art: Article) => {
+            const { body, title, id } = art
+            const brife = body.slice(0, 100)
+            steps.push({ id, label: title, description: brife, author: art.author, ttl: art.ttl })
         })
 
         setInprogressArts(steps)
@@ -40,15 +40,15 @@ const AcceptanceScreen = () => {
 
             {main}
 
-            {!!inProgressArts.length && !isLoading && <Box p={3}>
+            {!!inProgressArts.length && !isLoading && (<Box p={3}>
                 <Typography align="center" sx={{ fontWeight: 'bold' }}>Coming soon - articles in progress </Typography>
 
                 <Box sx={{ display: 'flex', justifyContent: 'center' }} m={2}>
                     <InProgressCarusel steps={inProgressArts} />
                 </Box>
-            </Box>}
+            </Box>)}
 
-            {isLoading && <AppProgress type="Line"/>}
+            {isLoading && <AppProgress type="Line" />}
         </Box>
     )
 }
--- src/screens/acceptance/InProgressCarusel.tsx
+++ src/screens/acceptance/InProgressCarusel.tsx
@@ -1,35 +1,36 @@
 import * as React from 'react'
-import Box from '@mui/material/Box'
+import { useNavigate } from 'react-router-dom'
+import { Box, MobileStepper, Paper, Typography, Button } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
-import MobileStepper from '@mui/material/MobileStepper'
-import Paper from '@mui/material/Paper'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft'
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight'
-import { StepItem } from './StepItem.interface'
-import { useNavigate } from 'react-router-dom'
-import { User } from '../../interface/user.interface'
-import TtlTimer from '../../utils/TimerSetter/TtlTimer'
+
 import useArticleQueries from '../../components/article/useArticleQueries'
+import TtlTimer from '../../utils/TimerSetter/TtlTimer'
+
+import { StepItem } from './StepItem.interface'
+import { User } from '../../interface/user.interface'
 import { UpdateTtl } from '../../api/dto/UpdateTtl.dto'
 
 type Props = {
     steps: StepItem[]
 }
 export default function InProgressCarusel(props: Props) {
-    const { steps } = props
-    const theme = useTheme()
     const [activeStep, setActiveStep] = React.useState(0)
+
+    const navigate = useNavigate()
+
+    const { steps } = props
     const maxSteps = steps.length
 
     const { increaseTtl } = useArticleQueries({})
 
-    const navigate = useNavigate()
+    const theme = useTheme()
 
     const handleNext = () => {
         setActiveStep((prevActiveStep) => prevActiveStep + 1)
     }
+
     const handleBack = () => {
         setActiveStep((prevActiveStep) => prevActiveStep - 1)
     }
@@ -44,8 +45,7 @@ export default function InProgressCarusel(props: Props) {
         increaseTtl.mutate(payload)
     }
 
-
-    const showAllAutherArticles = (author: User) => {
+    const showAllAuthorArticles = (author: User) => {
         navigate(`/cat/${author.first_name}-${author.last_name}/${author.id}`)
     }
 
@@ -54,42 +54,28 @@ export default function InProgressCarusel(props: Props) {
             <Typography><TtlTimer updateArtTtl={updateArtTtl} ttl={steps[activeStep].ttl!} /></Typography>
 
             <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+
                 <Paper
                     square
                     elevation={0}
-                    sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        height: 50,
-                        pl: 2,
-
-                        bgcolor: 'background.default',
-                    }}
+                    sx={paperStyle}
                 >
                     <Typography fontWeight='bold'>{steps[activeStep].label}</Typography>
                 </Paper>
 
                 <Box sx={{ maxWidth: 400, width: '100%', p: 2, display: 'grid' }}>
 
-                    <Typography sx={{ textDecoration: 'underline', cursor: 'pointer', borderRadius: '8px', width: '-webkit-fill-available', height: 'fit-content' }} onClick={() => showAllAutherArticles(steps[activeStep].author)}>
+                    <Typography
+                        sx={authorRowStyle}
+                        onClick={() => showAllAuthorArticles(steps[activeStep].author)}>
                         <span style={{ fontWeight: 'bold' }}>full name:</span> {steps[activeStep].author.first_name + ' ' + steps[activeStep].author.last_name}
                     </Typography>
 
-                    <Typography sx={{
-                        borderRadius: '8px',
-                        width: '-webkit-fill-available',
-                        height: 'fit-content',
-                    }} ><span style={{ fontWeight: 'bold' }}>email:</span>{steps[activeStep].author.email}</Typography>
-                    <Typography sx={{
-                        borderRadius: '8px',
-                        width: '-webkit-fill-available',
-                        height: 'fit-content',
-                    }}  ><span style={{ fontWeight: 'bold' }}>phone:</span> {steps[activeStep].author.phone_number}</Typography>
-                    <Typography sx={{
-                        borderRadius: '8px',
-                        width: '-webkit-fill-available',
-                        height: 'fit-content',
-                    }}  ><span style={{ fontWeight: 'bold' }}>role:</span>{steps[activeStep].author.role}</Typography>
+                    <ParameterRow data={steps[activeStep].author.email} label={'email'} />
+
+                    <ParameterRow data={steps[activeStep].author.phone_number} label={'phone'} />
+
+                    <ParameterRow data={steps[activeStep].author.role} label={'role'} />
 
                 </Box>
 
@@ -130,4 +116,36 @@ export default function InProgressCarusel(props: Props) {
             </Box>
         </Box>
     )
+}
+
+type ParamRow = {
+    label: string
+    data: any
+}
+const ParameterRow: React.FC<ParamRow> = (props) => {
+    return (
+        <Typography
+            sx={{
+                borderRadius: '8px',
+                width: '-webkit-fill-available',
+                height: 'fit-content',
+            }}>
+            <span style={{ fontWeight: 'bold' }}>{props.label}:</span>{props.data}</Typography>
+    )
+}
+
+const authorRowStyle = {
+    textDecoration: 'underline',
+    cursor: 'pointer',
+    borderRadius: '8px',
+    width: '-webkit-fill-available',
+    height: 'fit-content'
+}
+
+const paperStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    height: 50,
+    pl: 2,
+    bgcolor: 'background.default'
 }
