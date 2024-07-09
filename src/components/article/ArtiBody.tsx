ad9a7a15d25934b473d4079aa39f6d91b9c81192
meirjuli <mybs2323@gmail.com>
2024-07-09 21:55:36 +0300

    wip

--- src/components/article/ArtiBody.tsx
+++ src/components/article/ArtiBody.tsx
@@ -1,7 +1,7 @@
 import RegExpUtil from '../../utils/system/RegExp.util'
 import { Typography } from '@mui/material'
 import './style.css'
-import { RawDraftContentState } from 'draft-js'
+import { RawDraftContentState, DraftBlockType } from 'draft-js'
 
 type Props = {
     body: RawDraftContentState
@@ -10,12 +10,44 @@ type Props = {
 }
 
 const ArtiBody = (props: Props) => {
+    const renderBlock = (block: RawDraftContentState['blocks'][number]): JSX.Element => {
+        const { type, text, inlineStyleRanges } = block;
 
+        // 1. Determine the HTML element for the block type
+        let ElementType: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'blockquote' | 'pre' = 'p'; // Default to paragraph
+        switch (type) {
+            case 'header-one': ElementType = 'h1'; break;
+            case 'header-two': ElementType = 'h2'; break;
+            // ... handle other header types
+            case 'blockquote': ElementType = 'blockquote'; break;
+            case 'code-block': ElementType = 'pre'; break;
+        }
+
+        // 2. Apply inline styles (bold, italic, etc.) 
+        const styledText = inlineStyleRanges.reduce((acc, range) => {
+            const { offset, length, style } = range;
+            const styledSegment = <span style={{ fontWeight: style === 'BOLD' ? 'bold' : 'normal', fontStyle: style === 'ITALIC' ? 'italic' : 'normal' }}>{text.substring(offset, offset + length)}</span>;
+            return acc.concat(styledSegment);
+        }, [] as JSX.Element[]); // Start with an empty array of JSX elements
+
+        // 3. Render the block with the correct element and styled text
+        return (
+            <Typography component={ElementType} key={block.key}>
+                {styledText.length ? styledText : text} {/* If there are styled segments, use them, otherwise just the plain text */}
+            </Typography>
+        );
+    };
     // const paragraphs = Array.isArray(props.body) ? props.body : props.body.split(/[\n\r]+/)
-
     return (
         <main className='art-container'>
-            {props.body.blocks.map(({ text }, index) => {
+            {props.body.blocks.map(renderBlock)}
+            {/*</main>{props.body.blocks.map((block, index) => {
+                console.log({ block })
+                if (block.type === "header-three") {
+                    return (
+                        <h3>{block.text}</h3>
+                    )
+                }
                 // const isHeader: RegExpMatchArray | null = paragraph.match(RegExpUtil.headers)
 
                 // if (isHeader?.length) {
@@ -23,12 +55,12 @@ const ArtiBody = (props: Props) => {
                 return (
                     <Typography
                         gutterBottom
-                        component='h2'
+                        // component='h2'
                         paragraph={false}
                         key={index}
                         className='title-index'
                     >
-                        {text}
+                        {block.text}
                     </Typography>)
                 // } else if (!props.isedit) {
                 //     return (<Typography component='p' style={{ direction: RegExpUtil.rtl.test(paragraph) ? 'rtl' : 'ltr' }} key={index} className='body-paragraph'>{paragraph}</Typography>)
@@ -44,7 +76,7 @@ const ArtiBody = (props: Props) => {
                 //         </div>
                 //     )
                 // }
-            })}
+            // })}*/}
         </main>
     )
 }
